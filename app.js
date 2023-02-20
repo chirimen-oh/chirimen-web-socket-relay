2e92a4bdd0b8e8c1526dbdcf1d82347bebdf1699
Takashi SASAKI <takashi316@gmail.com>
2023-02-20 09:32:45 +0900

    VSCodeでフォーマット

--- app.js
+++ app.js
@@ -12,40 +12,40 @@ const wss = new ws.Server({ server: httpServer });
 
 var connections = [];
 
-wss.on("connection", function(ws, request ) {
-    console.log("Client connected request: origin:",request.headers.origin, " url:",request.url );
-    connections.push({socket:ws, url:request.url});
-    
+wss.on("connection", function (ws, request) {
+    console.log("Client connected request: origin:", request.headers.origin, " url:", request.url);
+    connections.push({ socket: ws, url: request.url });
+
     ws.on('close', function () {
         connections = connections.filter(function (conn, i) {
             var isTargetCon = (conn.socket === ws);
-//            if ( isTargetCon ){
-//                console.log("delete idx:",i, " url:",conn.url);
-//            }
+            //            if ( isTargetCon ){
+            //                console.log("delete idx:",i, " url:",conn.url);
+            //            }
             return (isTargetCon) ? false : true;
         });
     });
-    
+
     ws.on('message', function (message) {
-//        console.log('message:', message);
-        if ( message == "" || message == "ping" ){
+        //        console.log('message:', message);
+        if (message == "" || message == "ping") {
         } else {
-            try{
-                broadcast(JSON.stringify(JSON.parse(message)), request.url, ws );
-            } catch (e ){
+            try {
+                broadcast(JSON.stringify(JSON.parse(message)), request.url, ws);
+            } catch (e) {
                 // error skip
             }
         }
     });
 });
-    
-function broadcast(message, url, ws ) {
+
+function broadcast(message, url, ws) {
     connections.forEach(function (con, i) {
-        if ( con.url == url && con.socket !== ws ){ // Send same channel(url) only. However, no echo back to the sender.
-//            console.log("send:",con.url, " msg:",message);
+        if (con.url == url && con.socket !== ws) { // Send same channel(url) only. However, no echo back to the sender.
+            //            console.log("send:",con.url, " msg:",message);
             con.socket.send(message);
         } else {
-//            console.log("skip send:",con.url);        
+            //            console.log("skip send:",con.url);        
         }
     });
 };
